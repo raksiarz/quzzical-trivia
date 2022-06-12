81b2f8d209e9374a39ef8b097725ed7ab7748bde
Oskar <ostrowskioskar@gmail.com>
2022-06-13 00:37:17 +0200

    adding start game screen

---
+++ src/components/StartGame.js
@@ -0,0 +1,11 @@
+function StartGame() {
+    return (
+        <div className = "main--box">
+            <h2>Quzzical</h2>
+            <h4>Description</h4>
+            <button>Start Game</button>
+        </div>
+    )
+}
+
+export default StartGame
